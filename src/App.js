1402def2e4b9f119f639be4ac67fceb49d6213cc
Shea Cronin <shealcronin@gmail.com>
2021-05-05 16:45:31 -0400

    Add initial Section component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
 import './App.css';
+import React, { Component } from 'react';
 
 function App() {
-    return <div>hi</div>;
+    return (
+        <div>
+            <Section sectionName="Skills" />
+        </div>
+    );
+}
+
+class Section extends Component {
+    render() {
+        const { sectionName } = this.props;
+
+        return (
+            <div>
+                <h1>{sectionName}</h1>
+                <button>Edit</button>
+                <hr></hr>
+            </div>
+        );
+    }
 }
 
 export default App;
