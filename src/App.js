a8d246356ac22f742f4dc0243d2106d25d344586
Shea Cronin <shealcronin@gmail.com>
2021-05-05 17:17:59 -0400

    Create initial Information component

--- src/App.js
+++ src/App.js
@@ -4,10 +4,35 @@ import React, { Component } from 'react';
 function App() {
     return (
         <main>
+            <Information />
             <Section sectionName="Skills" />
             <Section sectionName="Experience" />
         </main>
     );
+}
+
+class Information extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            name: 'First Last',
+            email: 'firstlast@example.com',
+            phoneNumber: '123-456-7890',
+        };
+    }
+
+    render() {
+        const { name, email, phoneNumber } = this.state;
+
+        return (
+            <section>
+                <h1>{name}</h1>
+                <div>
+                    <span>{email}</span> | <span>{phoneNumber}</span>
+                </div>
+            </section>
+        );
+    }
 }
 
 class Section extends Component {
@@ -18,7 +43,7 @@ class Section extends Component {
             <section>
                 <h2>{sectionName}</h2>
                 <button>Edit</button>
-                <hr></hr>
+                <hr />
             </section>
         );
     }
