5db39ec7a96dce8b3b9424977561385429128d1a
Shea Cronin <shealcronin@gmail.com>
2021-05-10 18:29:18 -0400

    Add initial delete button styles and hover effects

--- src/components/DeleteButton.js
+++ src/components/DeleteButton.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import '../styles/DeleteButton.css';
+import deleteIcon from '../i/delete-icon.svg';
 
 const DeleteButton = (props) => {
     const { onClick } = props;
     return (
         <button onClick={onClick} className="delete-btn">
-            Delete
+            <img src={deleteIcon} alt="Delete icon" />
         </button>
     );
 };
