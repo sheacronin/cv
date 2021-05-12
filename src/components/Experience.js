20a4aeea0429a54e016723da9efad1dbb94c5b92
Shea Cronin <shealcronin@gmail.com>
2021-05-12 10:06:13 -0400

    Remove Skills and Experience components that did not use Section

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -1,11 +1,22 @@
 import React, { Component } from 'react';
 import '../styles/Experience.css';
-import EditButton from './EditButton';
-import SubmitButton from './SubmitButton';
 import DeleteButton from './DeleteButton';
 import Field from './Field';
 import uniqid from 'uniqid';
 import Section from './Section';
+
+class Experience extends Component {
+    render() {
+        return (
+            <Section
+                sectionTitle="Experience"
+                items={[jobFactory(), jobFactory()]}
+                ItemTag={Job}
+                itemFactory={jobFactory}
+            />
+        );
+    }
+}
 
 const jobFactory = () => {
     return {
@@ -22,100 +33,6 @@ const jobFactory = () => {
         id: uniqid(),
     };
 };
-
-class Experience extends Component {
-    constructor(props) {
-        super(props);
-
-        this.state = {
-            isEditable: false,
-            jobs: [
-                {
-                    title: 'Job Title',
-                    employer: 'Employer',
-                    location: 'City, State',
-                    startDate: 'Start Date',
-                    endDate: 'Present',
-                    bullets: [
-                        { text: 'Bullet 1', id: uniqid() },
-                        { text: 'Bullet 2', id: uniqid() },
-                        { text: 'Bullet 3', id: uniqid() },
-                    ],
-                    id: '123',
-                },
-                jobFactory(),
-            ],
-        };
-    }
-
-    handleEditClick = () => {
-        this.setState({
-            isEditable: true,
-        });
-    };
-
-    handleSubmitClick = () => {
-        this.setState({
-            isEditable: false,
-        });
-    };
-
-    render() {
-        const { jobs, isEditable } = this.state;
-
-        return (
-            <section>
-                <h2>Experience</h2>
-                {isEditable ? (
-                    <SubmitButton onClick={this.handleSubmitClick} />
-                ) : (
-                    <EditButton onClick={this.handleEditClick} />
-                )}
-                {jobs.map((job) => (
-                    <Job
-                        key={job.id}
-                        isEditable={isEditable}
-                        handleChange={this.handleChange}
-                        item={job}
-                    />
-                ))}
-                {isEditable && (
-                    <button id="new-skill" onClick={this.handleAddFieldClick}>
-                        +
-                    </button>
-                )}
-                <hr />
-            </section>
-        );
-    }
-}
-
-export class Experience2 extends Component {
-    render() {
-        return (
-            <Section
-                sectionTitle="Experience"
-                items={[
-                    {
-                        title: 'Job Title',
-                        employer: 'Employer',
-                        location: 'City, State',
-                        startDate: 'Start Date',
-                        endDate: 'Present',
-                        bullets: [
-                            { text: 'Bullet 1', id: uniqid() },
-                            { text: 'Bullet 2', id: uniqid() },
-                            { text: 'Bullet 3', id: uniqid() },
-                        ],
-                        id: '123',
-                    },
-                ]}
-                ItemTag={Job}
-                itemFactory={jobFactory}
-            />
-        );
-    }
-}
 
 class Job extends Component {
     render() {
--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,126 +1,16 @@
 import React, { Component } from 'react';
 import '../styles/Skills.css';
-import EditButton from './EditButton';
-import SubmitButton from './SubmitButton';
 import DeleteButton from './DeleteButton';
 import Field from './Field';
 import uniqid from 'uniqid';
 import Section from './Section';
 
 class Skills extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            isEditable: false,
-            skills: [
-                { text: 'Microsoft Word', id: uniqid() },
-                { text: 'JavaScript', id: uniqid() },
-            ],
-        };
-    }
-
-    handleChange = (e) => {
-        const { skills } = this.state;
-
-        // Make a copy of skills array.
-        const newSkills = skills;
-        const changeIndex = skills.findIndex(
-            (skill) => skill.id === e.target.className
-        );
-        const changedSkill = { text: e.target.value, id: e.target.className };
-        // Replace changed skill obj in array copy.
-        newSkills.splice(changeIndex, 1, changedSkill);
-
-        this.setState({
-            skills: newSkills,
-        });
-    };
-
-    handleAddFieldClick = () => {
-        const newSkill = { text: 'Skill', id: uniqid() };
-        this.setState({
-            skills: [...this.state.skills, newSkill],
-        });
-    };
-
-    handleEditClick = () => {
-        this.setState({
-            isEditable: true,
-        });
-    };
-
-    handleDeleteClick = (e) => {
-        const { skills } = this.state;
-
-        // Make a copy of skills array.
-        const newSkills = skills;
-        const deleteIndex = skills.findIndex(
-            (skill) => skill.id === e.currentTarget.dataset.field
-        );
-        // Mutate array copy by deleting selected skill.
-        newSkills.splice(deleteIndex, 1);
-
-        this.setState({
-            skills: newSkills,
-        });
-    };
-
-    handleSubmitClick = () => {
-        this.setState({
-            isEditable: false,
-        });
-    };
-
-    render() {
-        const { isEditable, skills } = this.state;
-
-        return (
-            <section id="skills">
-                <h2>Skills</h2>
-                {isEditable ? (
-                    <SubmitButton onClick={this.handleSubmitClick} />
-                ) : (
-                    <EditButton onClick={this.handleEditClick} />
-                )}
-                <ul>
-                    {skills.map((skill) => (
-                        <li key={skill.id}>
-                            {isEditable && (
-                                <DeleteButton
-                                    onClick={this.handleDeleteClick}
-                                    fieldId={skill.id}
-                                />
-                            )}
-                            <Field
-                                TagName="span"
-                                isEditable={isEditable}
-                                value={skill.text}
-                                handleChange={this.handleChange}
-                                className={skill.id}
-                            />
-                        </li>
-                    ))}
-                </ul>
-                {isEditable && (
-                    <button id="new-skill" onClick={this.handleAddFieldClick}>
-                        +
-                    </button>
-                )}
-                <hr />
-            </section>
-        );
-    }
-}
-
-export class Skills2 extends Component {
     render() {
         return (
             <Section
                 sectionTitle="Skills"
-                items={[
-                    { text: 'Microsoft Word', id: uniqid() },
-                    { text: 'JavaScript', id: uniqid() },
-                ]}
+                items={[skillFactory(), skillFactory()]}
                 ItemTag={Skill}
                 itemFactory={skillFactory}
             />
