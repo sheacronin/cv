6cf2ac29bdaafaec04e00611faf26ec012ab2563
Shea Cronin <shealcronin@gmail.com>
2021-06-07 15:30:20 -0400

    Refactor Education to use a functional component with hooks

--- src/components/Education.js
+++ src/components/Education.js
@@ -1,9 +1,12 @@
-import React, { Component } from 'react';
+import React from 'react';
 import '../styles/Education.css';
 import DeleteButton from './DeleteButton';
 import Field from './Field';
 import uniqid from 'uniqid';
-import Section from './Section';
+import EditButton from './EditButton';
+import SubmitButton from './SubmitButton';
+import useEditable from '../hooks/useEditable';
+import useItemsList from '../hooks/useItemsList';
 
 function schoolFactory() {
     return {
@@ -15,68 +18,95 @@ function schoolFactory() {
     };
 }
 
-class Education extends Component {
-    render() {
-        return (
-            <Section
-                sectionTitle="Education"
-                items={[schoolFactory()]}
-                ItemTag={School}
-                itemFactory={schoolFactory}
-            />
-        );
-    }
+function Education() {
+    const [isEditable, handleEditClick] = useEditable();
+
+    const [schools, dispatch] = useItemsList(schoolFactory, 1);
+
+    return (
+        <section id="education">
+            <h2>Education</h2>
+            {isEditable ? (
+                <SubmitButton onClick={handleEditClick} />
+            ) : (
+                <EditButton onClick={handleEditClick} />
+            )}
+            {schools.map((school) => (
+                <School
+                    key={school.id}
+                    isEditable={isEditable}
+                    item={school}
+                    handleChange={(e) =>
+                        dispatch({
+                            type: 'edit',
+                            id: school.id,
+                            value: e.target.value,
+                            // Use class name because attr
+                            // is listed there.
+                            attr: e.target.className,
+                        })
+                    }
+                    handleDeleteClick={() =>
+                        dispatch({ type: 'delete', id: school.id })
+                    }
+                />
+            ))}
+            {isEditable && (
+                <button
+                    className="new-item-btn"
+                    onClick={() => dispatch({ type: 'add' })}
+                >
+                    +
+                </button>
+            )}
+            <hr />
+        </section>
+    );
 }
 
-class School extends Component {
-    render() {
-        const { item, isEditable, handleChange, handleDeleteClick } =
-            this.props;
+function School(props) {
+    const { item, isEditable, handleChange, handleDeleteClick } = props;
 
-        return (
-            <article className="school">
-                {isEditable && (
-                    <DeleteButton
-                        onClick={handleDeleteClick}
-                        fieldId={item.id}
-                    />
-                )}
-                <h3>
-                    <Field
-                        isEditable={isEditable}
-                        value={item.institution}
-                        handleChange={handleChange}
-                        itemId={item.id}
-                        attribute={'institution'}
-                    />
-                    {', '}
-                    <Field
-                        isEditable={isEditable}
-                        value={item.location}
-                        handleChange={handleChange}
-                        itemId={item.id}
-                        attribute={'location'}
-                    />
-                </h3>
+    return (
+        <article className="school">
+            {isEditable && (
+                <DeleteButton onClick={handleDeleteClick} fieldId={item.id} />
+            )}
+            <h3>
                 <Field
                     isEditable={isEditable}
-                    value={item.degree}
+                    value={item.institution}
                     handleChange={handleChange}
                     itemId={item.id}
-                    attribute={'degree'}
+                    attribute={'institution'}
                 />
-                <div className="dates">
-                    <Field
-                        isEditable={isEditable}
-                        value={item.gradDate}
-                        handleChange={handleChange}
-                        itemId={item.id}
-                        attribute={'gradDate'}
-                    />
-                </div>
-            </article>
-        );
-    }
+                {', '}
+                <Field
+                    isEditable={isEditable}
+                    value={item.location}
+                    handleChange={handleChange}
+                    itemId={item.id}
+                    attribute={'location'}
+                />
+            </h3>
+            <Field
+                isEditable={isEditable}
+                value={item.degree}
+                handleChange={handleChange}
+                itemId={item.id}
+                attribute={'degree'}
+            />
+            <div className="dates">
+                <Field
+                    isEditable={isEditable}
+                    value={item.gradDate}
+                    handleChange={handleChange}
+                    itemId={item.id}
+                    attribute={'gradDate'}
+                />
+            </div>
+        </article>
+    );
 }
 
 export default Education;
