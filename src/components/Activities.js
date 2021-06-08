57530e16cb578ec5ecadcb72ccb768d8233ee064
Shea Cronin <shealcronin@gmail.com>
2021-06-08 11:51:00 -0400

    Refator Activities to use hooks

--- src/components/Activities.js
+++ src/components/Activities.js
@@ -1,21 +1,10 @@
-import React, { Component } from 'react';
+import React from 'react';
 import uniqid from 'uniqid';
-import Section from './Section';
 import Bullet from './Bullet';
-
-class Activities extends Component {
-    render() {
-        return (
-            <Section
-                sectionTitle="Activities"
-                items={[activityFactory(), activityFactory()]}
-                ItemTag={Bullet}
-                itemFactory={activityFactory}
-                isLastSection="true"
-            />
-        );
-    }
-}
+import useItemsList from '../hooks/useItemsList';
+import useEditable from '../hooks/useEditable';
+import SubmitButton from './SubmitButton';
+import EditButton from './EditButton';
 
 const activityFactory = () => {
     return {
@@ -23,5 +12,50 @@ const activityFactory = () => {
         id: uniqid(),
     };
 };
+
+function Activities() {
+    const [isEditable, handleEditClick] = useEditable();
+    const [activities, dispatch] = useItemsList(activityFactory, 2);
+
+    return (
+        <section id="activities">
+            <h2>Activities</h2>
+            {isEditable ? (
+                <SubmitButton onClick={handleEditClick} />
+            ) : (
+                <EditButton onClick={handleEditClick} />
+            )}
+            <ul>
+                {activities.map((activity) => (
+                    <Bullet
+                        key={activity.id}
+                        isEditable={isEditable}
+                        item={activity}
+                        handleChange={(e) =>
+                            dispatch({
+                                type: 'edit',
+                                id: activity.id,
+                                value: e.target.value,
+                                attr: 'text',
+                            })
+                        }
+                        handleDeleteClick={() =>
+                            dispatch({ type: 'delete', id: activity.id })
+                        }
+                        isMultiline={false}
+                    />
+                ))}
+            </ul>
+            {isEditable && (
+                <button
+                    className="new-item-btn"
+                    onClick={() => dispatch({ type: 'add' })}
+                >
+                    +
+                </button>
+            )}
+        </section>
+    );
+}
 
 export default Activities;
